61de60ea47022ae9d31ef03f541d7f64bad5e6ff
alisson.co <alisson_oliveira_11@hotmail.com>
2022-01-28 20:54:01 -0300

    adding features and fixing bugs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+/* Show Menuc*/
 const navMenu = document.getElementById('nav_menu'),
       navToggle = document.getElementById('nav_toggle'),
       navClose = document.getElementById('nav_close')
@@ -8,8 +9,19 @@ if(navToggle){
     })
 }
 
+/* Menu Hidden */
 if(navClose){
     navClose.addEventListener('click', () =>{
         navMenu.classList.remove('show-menu')
     })
 }
+
+/* Remove Menu Mobile */
+const navLink = document.querySelectorAll('.nav-link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav_menu')
+    // remove a class show-menu ao clicar em um link
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
